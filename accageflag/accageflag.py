e2865dd5874c4c35299177906b36da69c992bc07
ZephyrTFA <matthew@tfaluc.com>
2022-07-12 14:40:55 -0400

    accage

---
+++ accageflag/accageflag.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+import discord
+from redbot.core import commands, Config, checks
+
+class AccountAgeFlagger(commands.Cog):
+	"""Class to manage flagging accounts under a specified age"""
+
+	def __init__(self, bot: commands.Bot) -> None:
+		self.bot = bot
+		self.config = Config.get_conf(self, identifier=51360816380568, force_registration=True)
+
+		def_guild = {
+			"needs_verification_role": None,
+			"needs_verification_log": None,
+			"verifier_role": None,
+			"account_age_minimum_days": 15,
+		}
+		self.config.register_guild(**def_guild)
+	
+	@commands.Cog.listener()
+	async def on_member_join(self, ctx: commands.Context, member: discord.Member):
+		day_cutoff: int = self.config.guild(ctx.guild).account_age_minimum_days()
+		mem_age: datetime = member.created_at
+		mem_delta: timedelta = mem_age - datetime.now()
+		if(mem_delta.days > day_cutoff):
+			return
+		
+		guild: discord.Guild = ctx.guild
+		
+		role: discord.Role = guild.get_role(self.config.guild(ctx.guild).needs_verification_role())
+		await member.add_roles(role)
+
+		verifier_role: discord.Role = guild.get_role(self.config.guild(ctx.guild).verifier_role())
+		channel: discord.TextChannel = guild.get_channel(self.config.guild(ctx.guild).needs_verification_log())
+		channel.send("[VERIFICATION]: {} is only {} days old! {}".format(member.mention, mem_delta.days, verifier_role.mention))
+
+	@commands.command()
+	@checks.admin()
+	async def test_command(self, ctx: commands.Context):
+		member: discord.Member = ctx.author
+
+		day_cutoff: int = self.config.guild(ctx.guild).account_age_minimum_days()
+		mem_age: datetime = member.created_at
+		mem_delta: timedelta = mem_age - datetime.now()
+		if(mem_delta.days > day_cutoff):
+			return
+		
+		guild: discord.Guild = ctx.guild
+		
+		role: discord.Role = guild.get_role(self.config.guild(ctx.guild).needs_verification_role())
+		await member.add_roles(role)
+
+		verifier_role: discord.Role = guild.get_role(self.config.guild(ctx.guild).verifier_role())
+		channel: discord.TextChannel = guild.get_channel(self.config.guild(ctx.guild).needs_verification_log())
+		channel.send("[VERIFICATION]: {} is only {} days old! {}".format(member.mention, mem_delta.days, verifier_role.mention))
