c09c729521871f171f226f5ea8ed2d26b32a8bc1
ZephyrTFA <matthew@tfaluc.com>
2022-07-13 21:46:49 -0400

    debug

--- ss13mon/ss13mon.py
+++ ss13mon/ss13mon.py
@@ -138,9 +138,11 @@ class SS13Mon(commands.Cog):
 			return
 		
 		message = await cfg.message_id()
+		await channel.send("DBG: old message id = `{}`".format(message))
 		cached: discord.Message
 		if(message == None):
 			cached = await channel.send("caching initial context")
+			await channel.send("DBG: new message id = `{}`".format(cached.id))
 			await cfg.message_id.set(cached.id)
 		else:
 			try:
